503c5ee79e3611f36a37ab1a8e547babb57f075f
Tom Claessens <tom.claessens@student.kdg.be>
2019-03-12 23:05:54 +0100

    index changes

--- CityOfIdeas/CityOfIdeas/Controllers/HomeController.cs
+++ CityOfIdeas/CityOfIdeas/Controllers/HomeController.cs
@@ -12,5 +12,24 @@ namespace CityOfIdeas.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
