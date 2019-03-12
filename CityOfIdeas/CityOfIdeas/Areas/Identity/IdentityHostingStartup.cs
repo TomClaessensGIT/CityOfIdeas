c7002d8dff140c46b4d36622e2b6d9a6b232b1c9
IliasEM <47138417+IliasEM@users.noreply.github.com>
2019-03-12 21:14:01 +0100

    e

---
+++ CityOfIdeas/CityOfIdeas/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CityOfIdeas.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CityOfIdeas.Areas.Identity.IdentityHostingStartup))]
+namespace CityOfIdeas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
