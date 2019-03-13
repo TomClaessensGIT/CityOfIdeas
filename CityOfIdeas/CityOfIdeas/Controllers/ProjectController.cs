bc08eace08646cccb8826bfb76265f9d46d4dfc9
IliasEM <47138417+IliasEM@users.noreply.github.com>
2019-03-13 12:01:32 +0100

    Index aangepast, register pagina toegevoegd, links toegevoegd naar projecten pagina

--- CityOfIdeas/CityOfIdeas/Controllers/ProjectController.cs
+++ CityOfIdeas/CityOfIdeas/Controllers/ProjectController.cs
@@ -6,14 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityOfIdeas.Controllers
 {
-    public class HomeController : Controller
+    public class ProjectController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
         {
             return View();
         }
