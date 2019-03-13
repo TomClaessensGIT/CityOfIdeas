c1456198faecefd55637c6dc313caf667d9c5adb
Pieter <pieter.joos@student.kdg.be>
2019-03-13 15:01:35 +0100

    vragenlijst

---
+++ CityOfIdeas/CityOfIdeas/Models/Vragenlijst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityOfIdeas.Models
+{
+    public class Vragenlijst
+    {
+        public int Id { get; set; }
+        //public Vraag vraag
+    }
+}
