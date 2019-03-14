da7ff970e01c38d26bb2c9444b1043e42514e9e5
Pieter <pieter.joos@student.kdg.be>
2019-03-14 19:51:29 +0100

    meer ideations

--- CityOfIdeas/DAL/EF/CityOfIdeasDbInitializer.cs
+++ CityOfIdeas/DAL/EF/CityOfIdeasDbInitializer.cs
@@ -164,6 +164,22 @@ namespace CID.DAL.EF
                 Ideations = new List<Ideation>()
 
             };
+
+
+            Ideation ideation5 = new Ideation()
+            {
+                Project = project4,
+                Title = "verkeersdrempel",
+                Text = "Aangezien deze straten nu ook fietsstraten zijn geworden zou ik graag willen dat er in elke straat meerdere verkeersdrempels komen zodat de auto's op hun snelheid moeten letten",
+                Image = "images/portfolio/ideations/flitspalen.jpg",
+                Tags = new List<Tag>()
+
+
+            };
+
+            ideation5.Tags.Add(tag2);
+            project4.Ideations.Add(ideation5);
+
             Project project5 = new Project()
             {
                 Title = "Met z’n allen naar een mooie, groene buurt in Mariaburg.",
@@ -178,6 +194,18 @@ namespace CID.DAL.EF
                 Image = "images/portfolio/thumbnails/mariaBurg.jpg",
                 Ideations = new List<Ideation>()
             };
+
+            Ideation ideation6 = new Ideation()
+            {
+                Project = project5,
+                Title = "meer vuilbakken",
+                Text = "Aangezien deze straten nu ook fietsstraten zijn geworden zou ik graag willen dat er in elke straat meerdere verkeersdrempels komen zodat de auto's op hun snelheid moeten letten",
+                Image = "images/portfolio/ideations/flitspalen.jpg",
+                Tags = new List<Tag>()
+
+
+            };
+
             Project project6 = new Project()
             {
                 Title = "Groenplan zorgt voor 187 hectaren extra groen in de stad.",
