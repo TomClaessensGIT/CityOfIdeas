f14d2b4f55d1d60b2e4c232a04b35f6e665d36c9
Tom Claessens <tom.claessens@student.kdg.be>
2019-03-14 15:58:56 +0100

    Projecten en ideation aangemaakt  + database voorloopig

---
+++ CityOfIdeas/BL/ProjectManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CID.BL.Domain;
+using CID.DAL;
+using CID.DAL.EF;
+
+namespace CID.BL
+{
+   public class ProjectManager : IProjectManager
+    {
+        private readonly IProjectRepository repo;
+
+        public ProjectManager()
+        {
+            //repo = new TicketRepositoryHC();
+            repo = new ProjectRepository();
+        }
+        public Ideation AddIdeation(int projectNumber, string title, string text, ICollection<TagEnum> tags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project AddProject(string titel, string preface, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeProject(Project ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ideation> GetIdeations(int projectNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetProject(int ticketNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveProject(int ticketNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CityOfIdeas/Domain/TagEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CID.BL.Domain
+{
+    public enum TagEnum
+    {
+        [Display(Name = "Natuur")]
+        Natuur,
+        [Display(Name = "Plein")]
+        Plein,
+        [Display(Name = "Antwerpen")]
+        Antwerpen,
+        [Display(Name = "Deurne")]
+        Deurne,
+        [Display(Name = "Kinderen")]
+        Kinderen,
+        [Display(Name = "Verkeer")]
+        Verkeer
+    }
+}
